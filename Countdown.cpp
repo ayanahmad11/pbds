5834653eb60743a71c7e5c0f7bd0c263e7693e9b
ayanahmad11 <154846395+ayanahmad11@users.noreply.github.com>
2024-02-24 13:39:34 +0530

    DIv3 E. // digit sum // reverse ans // carry addition

--- Countdown.cpp
+++ Countdown.cpp
@@ -30,32 +30,83 @@ template <typename T, typename... V> void _print(T t, V... v) {__print(t); if (s
 
 void ayan(){
 int n;
-        cin >> n;
-        string s;
-        cin >> s;
-        reverse(s.begin(), s.end());
-        vector<int> a(n + 1);
-        for (int i = n - 1; i >= 0; i--) {
-            a[i] = a[i + 1] + (s[i] - '0');
+    std::cin >> n;
+    
+    std::string s;
+    std::cin >> s;
+    
+    std::reverse(s.begin(), s.end());
+    
+    // removed all zeroes from front side of original number 
+    
+    while (s.back() == '0') {
+        s.pop_back();
+        n--;
+    }
+    
+    debug(s);
+    
+    
+    // divided all digits in a number into a vector
+    std::vector<int> f(n);
+    for (int i = 0; i < n; i++) {
+        f[i] = s[i] - '0';
+    }
+    debug(f); 
+    
+    // calc dig sum
+    
+    for (int i = n - 2; i >= 0; i--) {
+        f[i] += f[i + 1];
+    }
+    
+    debug(f);
+    
+    
+    for (int i = 0; i < n; i++) {
+        if (f[i] >= 10) {
+            if (i == n - 1) {
+                n++;
+                f.push_back(0);
+            }
+           // ADDING CARRY
+            f[i + 1] += f[i] / 10;
+            f[i] %= 10;
         }
-        debug(a);
-        string res;
-        int c = 0;
-  // 3rd part
-        for (int i = 0; i < n; i++) {
-            c += a[i];
-            res += (char)(c % 10 + '0');
-            debug(res);
+    }
+    debug(f);
+    std::reverse(f.begin(), f.end());
+    for (int i = 0; i < n; i++) {
+        std::cout << f[i];
+    }
+    std::cout << "\n";
+	// int n;
+//         cin >> n;
+//         string s;
+//         cin >> s;
+//         reverse(s.begin(), s.end());
+//         vector<int> a(n + 1);
+//         for (int i = n - 1; i >= 0; i--) {
+//             a[i] = a[i + 1] + (s[i] - '0');
+//         }
+//         debug(a);
+//         string res;
+//         int c = 0;
+//   // 3rd part
+//         for (int i = 0; i < n; i++) {
+//             c += a[i];
+//             res += (char)(c % 10 + '0');
+//             debug(res);
            
-            c /= 10;    
-             debug(c);
-        }
-        res += (char)(c + '0');
-        while (res.back() == '0') {
-            res.pop_back();
-        }
-        reverse(res.begin(), res.end());
-        cout << res << "\n";
+//             c /= 10;    
+//              debug(c);
+//         }
+//         res += (char)(c + '0');
+//         while (res.back() == '0') {
+//             res.pop_back();
+//         }
+//         reverse(res.begin(), res.end());
+//         cout << res << "\n";
 }
 
 
