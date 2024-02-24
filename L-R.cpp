99b93c36e649dfa030e02b43fd4e492b5f1945f8
ayanahmad11 <154846395+ayanahmad11@users.noreply.github.com>
2024-02-24 12:53:58 +0530

    Dequeue // reverse ans // Modular multiplication

---
+++ L-R.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+#define int long long int
+#define vi  vector<int>
+#define vp  vector<pair<int,int>>
+#define pb  push_back
+#define all(v) v.begin(),v.end()
+#define fl(i,a,n)  for(int i = (a); i < (n); i++)
+#define ff  first
+#define ss  second
+#define pb push_back 
+#define sz(x) (int)x.size()
+#define mod 1000000007
+/*--- debuger --- */    
+void __print(int32_t x) {cerr << x;} void __print(long x) {cerr << x;} void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;} void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;} void __print(float x) {cerr << x;} 
+void __print(double x) {cerr << x;} void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';} void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';} void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+void ayan(){
+int n,m;
+cin>>n>>m;
+
+deque<int> a;
+fl(i,0,n)
+{
+	int c;
+	cin>>c;
+	a.pb(c);
+	
+}
+
+string s;
+cin>>s;
+ // debug(a);
+ // debug(s);
+ vi v;
+for(auto c :s){
+	if(c == 'L' ){
+		
+		
+		v.pb(a.front());
+			a.pop_front();
+	}
+	
+	else{
+		v.pb(a.back());
+		a.pop_back();
+	}		
+}
+reverse(all(v));
+// debug(v);
+vi ans;
+int mul=1;
+for(auto x:v){
+	mul*=x;
+	mul=mul%m;
+	ans.pb(mul);
+}
+reverse(all(ans));
+// debug(ans);
+for(auto x: ans){
+	cout<<x<<" ";
+}
+cout<<endl;
+
+}
+
+
+int32_t main(){
+ ios_base::sync_with_stdio(0);
+    cin.tie(0);
+int tt;
+cin>>tt;
+
+while(tt--){
+
+	ayan();
+}
+
+return 0;
+} 
