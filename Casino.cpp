45dc8b015785987150fc9b559c0381b700637bfc
ayanahmad11 <154846395+ayanahmad11@users.noreply.github.com>
2024-02-26 21:45:57 +0530

    // 2-d array // transpose // sortings // prefix and suffix

---
+++ Casino.cpp
@@ -0,0 +1,82 @@
+// https://codeforces.com/contest/1808/problem/B
+// Playing in casino B
+// 2-d array // transpose // sortings // prefix and suffix 
+
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+#define int long long int
+#define vi  vector<int>
+#define vp  vector<pair<int,int>>
+#define pb  push_back
+#define all(v) v.begin(),v.end()
+#define fl(i,a,n)  for(int i = (a); i < (n); i++)
+#define ff  first
+#define ss  second
+#define pb push_back 
+#define sz(x) (int)x.size()
+#define mod 1000000007
+/*--- debuger --- */    
+void __print(int32_t x) {cerr << x;} void __print(long x) {cerr << x;} void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;} void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;} void __print(float x) {cerr << x;} 
+void __print(double x) {cerr << x;} void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';} void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';} void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+void ayan(){
+int n,m;
+cin>>n>>m;
+
+vector<vector<int>> v(m,vector<int> (n));
+
+// debug(v);
+
+// transposing the 2-d array given
+
+fl(i,0,n){
+	fl(j,0,m){	
+		cin>>v[j][i];
+	}
+}
+int ans=0;
+fl(i,0,m){
+	sort(all(v[i]));
+	// debug(v[i]);
+	
+	int sum = v[i][0];
+	
+	fl(j,1,n){
+		
+		ans+= abs((j*v[i][j])-sum);
+		sum+=v[i][j];
+		
+   
+     }
+     // debug(ans);
+
+   }
+   cout<<ans<<endl;
+}
+
+
+int32_t main(){
+ ios_base::sync_with_stdio(0);
+    cin.tie(0);
+int tt;
+cin>>tt;
+
+while(tt--){
+
+	ayan();
+}
+
+return 0;
+} 
