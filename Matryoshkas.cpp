505e0b3287eb346cca56ced49a4ea4f09befd673
ayanahmad11 <154846395+ayanahmad11@users.noreply.github.com>
2024-02-26 22:41:49 +0530

    Good use of multiset

---
+++ Matryoshkas.cpp
@@ -0,0 +1,82 @@
+// https://codeforces.com/problemset/problem/1790/D
+//  Good use of multiset multiset
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+#define int long long int
+#define vi  vector<int>
+#define vp  vector<pair<int,int>>
+#define pb  push_back
+#define all(v) v.begin(),v.end()
+#define fl(i,a,n)  for(int i = (a); i < (n); i++)
+#define ff  first
+#define ss  second
+#define pb push_back 
+#define sz(x) (int)x.size()
+#define mod 1000000007
+/*--- debuger --- */    
+void __print(int32_t x) {cerr << x;} void __print(long x) {cerr << x;} void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;} void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;} void __print(float x) {cerr << x;} 
+void __print(double x) {cerr << x;} void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';} void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';} void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+void ayan(){
+
+int n;
+cin >>n;
+
+vi v(n);
+fl(i,0,n)
+cin>>v[i];
+
+
+    multiset<int> st(all(v));
+    // debug(st);
+    int ans = 0;
+    while(st.size()) {
+        auto it = st.end();
+        --it;
+        int x = *it;
+        ans++;
+        // debug(x);
+        
+        // while multiset conatins that element erase that element
+        // and lower that element
+    	//if the element when lowered by one does not 
+    	// exist in multiset it means we have to add in another group
+    
+        // cout<<(st.find(x)!=st.end())<<"v"<<endl;
+        while(st.find(x)!=st.end()){
+            st.erase(st.find(x));
+            --x;
+        }
+    }
+    cout << ans << "\n";
+
+
+
+}
+
+
+int32_t main(){
+ ios_base::sync_with_stdio(0);
+    cin.tie(0);
+int tt;
+cin>>tt;
+
+while(tt--){
+
+	ayan();
+}
+
+return 0;
+} 
