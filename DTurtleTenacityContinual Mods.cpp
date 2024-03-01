00b5b187821f46a165e09a58c2ae8f5fedf21914
ayanahmad11 <154846395+ayanahmad11@users.noreply.github.com>
2024-03-01 20:05:34 +0530

    freq // mod

---
+++ DTurtleTenacityContinual Mods.cpp
@@ -0,0 +1,73 @@
+//https://codeforces.com/contest/1933/problem/D
+// Look for Smallest element if it occurs once ans is always possible
+// else if look for mod of any element with small el not equal to 0
+// else not possible
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+#define int long long int
+#define vi  vector<int>
+#define vp  vector<pair<int,int>>
+#define pb  push_back
+#define all(v) v.begin(),v.end()
+#define fl(i,a,n)  for(int i = (a); i < (n); i++)
+#define ff  first
+#define ss  second
+#define pb push_back 
+#define sz(x) (int)x.size()
+#define mod 1000000007
+/*--- debuger --- */    
+void __print(int32_t x) {cerr << x;} void __print(long x) {cerr << x;} void __print(long long x) {cerr << x;} void __print(unsigned x) {cerr << x;} void __print(unsigned long x) {cerr << x;} void __print(unsigned long long x) {cerr << x;} void __print(float x) {cerr << x;} 
+void __print(double x) {cerr << x;} void __print(long double x) {cerr << x;} void __print(char x) {cerr << '\'' << x << '\'';} void __print(const char *x) {cerr << '\"' << x << '\"';} void __print(const string &x) {cerr << '\"' << x << '\"';} void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V> void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T> void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V> void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+void ayan(){
+	int n;
+	cin>>n;
+    vi v(n);
+    fl(i,0,n)
+    cin>>v[i];
+	int mn= *min_element(all(v));
+	if(count(all(v),mn)==1)
+	{
+		cout<<"Yes"<<endl;
+		return;
+	}
+	for(int i=0;i<n;i++){
+		
+		if(v[i]%mn!=0)
+		{
+			cout<<"Yes"<<endl;
+			return;
+		}
+		
+		
+	}
+	cout<<"no"<<endl;
+	
+	}
+
+
+int32_t main(){
+ ios_base::sync_with_stdio(0);
+    cin.tie(0);
+int tt;
+cin>>tt;
+
+while(tt--){
+
+	ayan();
+}
+
+return 0;
+} 
